f89161a7ba9806bafc19ddcc52652af49da9315e
Khavushka <khavushka@gmail.com>
2023-02-26 20:51:48 +0100

    Factorial formula

---
+++ simple_recursive_algo.py
@@ -0,0 +1,9 @@
+# factorials
+'''
+Factorial formula for n = 5
+n! = (n-0)*(n-1)*(n-2)*(n-3)*(n-4)
+
+factorials only involve positive integers so 
+once you hit one the program terminates 
+120 = (5)*(4)*(3)*(2)*(1)
+'''
