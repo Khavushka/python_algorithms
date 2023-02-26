cd877f121272441878e464d7da4eebb8546106bb
Khavushka <khavushka@gmail.com>
2023-02-26 20:37:55 +0100

    Introduction

--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-# just beginning
+# bossed around 
+
+# Introductions algorithms
+'''
+1. Simple recursive algorithms
+2. Algoriths within data structures
+3. Divide & conquer
+4. Greedy algorithms
+5. Dynamic programming
+'''
+
+'''
+- writing functions
+- variables
+- conditions (if/else)
+- for loops
+- basic syntax
+'''
+
