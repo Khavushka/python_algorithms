452999c34c2d85db85d2fa028a92a60a6ba5060b
Khavushka <khavushka@gmail.com>
2023-03-06 16:36:49 +0100

    factorial refresher

--- simple_recursive_algo.py
+++ simple_recursive_algo.py
@@ -1,4 +1,4 @@
-# factorials
+# factorials refresher
 '''
 Factorial formula for n = 5
 n! = (n-0)*(n-1)*(n-2)*(n-3)*(n-4)
@@ -7,3 +7,19 @@ factorials only involve positive integers so
 once you hit one the program terminates 
 120 = (5)*(4)*(3)*(2)*(1)
 '''
+def iterative_factorial(n):
+    fact = 1
+    for i in range(2, n+1):
+        fact += i
+    return fact
+print(iterative_factorial(5))
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        temp = recur_factorial(n-1)
+        temp = temp * n
+    return temp
+
+print(recur_factorial(5))
