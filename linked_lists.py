bd1e3a187c1a81fc1a6724a623aed9f17f187644
Khavushka <khavushka@gmail.com>
2023-03-07 19:22:37 +0100

    Description Linked lists

---
+++ linked_lists.py
@@ -0,0 +1,19 @@
+# Linked lists (traverse, search, add, delete, header, nodes, tail)
+'''
+A linked list is a linear collection of data elements whose order is 
+not given by their physical placement in memory. Instead, 
+each element points to the next. It's a data structure consisting of 
+a collection of nodes which together represent a sequence.
+
+In its most basic form, each node contains: data, and a reference
+(in other words, a link) to the next node in the sequence. 
+
+This structure allows for efficicent insertion or removal of elements 
+from any position in the sequence during iteration. 
+
+More complex variants add additional links, allowing more efficient
+insertion or removal of nodes at arbitrary positions.
+
+A drawback of linked lists is that access time is linear (and difficult to pipeline)
+'''
+
