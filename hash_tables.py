6d91c86b4d5c20ba3628788e6d39046e570a2137
Khavushka <khavushka@gmail.com>
2023-03-08 06:26:47 +0100

    Hash tables| collisiśion

---
+++ hash_tables.py
@@ -0,0 +1,26 @@
+'''
+Hash tables:
+- associative arrays
+- hash functions
+- key/value pairs
+- collision, and
+- chaining
+
+Used to:
+- store
+- retrive
+- delete
+
+Have to support 3 functions:
+- insert (key, value) 
+- get (key)
+- delete (key)
+
+Collission ispolzuetsya kogda proishodit,
+kakoe-to ne shodstvo raznyh dannyh
+'''
+
+# Rabin-Karp algorithm for pattern searching
+# matches the hash value of the pattern with 
+# the hash value of the current substring of text
+
