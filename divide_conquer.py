22bf25343dc4bfef3688d8a0666621f04cdd3cfe
Khavushka <khavushka@gmail.com>
2023-03-10 10:15:52 +0100

    Append to empty C

--- divide_conquer.py
+++ divide_conquer.py
@@ -13,4 +13,22 @@ print(C)
 
 
 # second solution merge
-def merging
+def merging(left, right):
+    C = []
+    while min(len(left), len(right)) > 0:
+        if left[0] > right[0]:
+            insert = right.pop(0)
+            C.append(insert)
+        elif left[0] <= right[0]:
+            insert = left.pop(0)
+            C.append(insert)
+    if len(left) > 0:
+        for i in left:
+            C.append(i)
+    if len(right) > 0:
+        for i in right:
+            C.append(i)
+        return C
+left = [2, 5, 6, 10]
+right = [3, 4, 12, 20]
+print(merging(left, right))
