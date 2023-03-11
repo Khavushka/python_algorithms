a3adb290289f3daedfdb08931f68b08c10af8bdb
Khavushka <khavushka@gmail.com>
2023-03-11 17:20:55 +0100

    Dynamic programming

---
+++ dynamic_prog.py
@@ -0,0 +1,8 @@
+# Dynamic Programming
+
+'''
+Python  is strongly typed as the interpreter keeps
+track of all variables types. Its's also very dymanic as it rarely
+uses what it knows to limit variable usage
+'''
+
