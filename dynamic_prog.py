76ce90512c73e7423badb76738b76a8da425076b
Khavushka <khavushka@gmail.com>
2023-03-11 17:42:51 +0100

    Ugly-number

--- dynamic_prog.py
+++ dynamic_prog.py
@@ -4,5 +4,15 @@
 Python  is strongly typed as the interpreter keeps
 track of all variables types. Its's also very dymanic as it rarely
 uses what it knows to limit variable usage
+- breaking the problem down
+- solving recursively (рекурсивно)
+- optimal solutions to construct optimal solutions
+
+In dynamic progaramming there are three basic methods:
+- recursion
+- top-down(w/memorization)
+- bottom-up(w/tabulation)
 '''
+
+# Ugly-number
 
