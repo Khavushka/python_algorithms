72abfdc39bb1ebcf58dfa5e82c674fc5abc33d30
Khavushka <khavushka@gmail.com>
2023-03-11 19:39:22 +0100

    Palindrome path

--- pal_mat.py
+++ pal_mat.py
@@ -11,3 +11,49 @@ def paths(m, n):
         print(row)
     return row[0]
 print(paths(3, 3))
+
+print("\n================================")
+
+import itertools
+a = [['a', 'a', 'a'],
+     ['b', 'b', 'b'],
+     ['a', 'b', 'a']]
+x = list(itertools.product(*a))
+# print(x)
+
+p_1 = []
+for i in x:
+    y = "".join(i)
+    p_1.append(y)
+# print(p_1)
+# print(set(p_1))
+
+def isPalin_2(p_1):
+    p_2 = []
+    for x in set(p_1):
+        if x == x[::-1]:
+            p_2.append(x)
+    return p_2
+print(isPalin_2(p_1))
+
+print("\n================================")
+
+def is_palindrome(string):
+    if string == string[::-1]:
+        return True
+    else:
+        return False
+    
+def palindromic_path(string, a, i, j, m, n):
+    if j < m -1 or i < n - 1:
+        if i < n - 1:
+            palindromic_path(string + a[i][j], a, i + 1, j, m, n)
+        if j < m -1:
+            palindromic_path(string + a[i][j], a, i, j + 1, m, n)
+    else:
+        string = string + a[n - 1][m - 1]
+        if is_palindrome(string):
+            print(string)
+str = ""
+print(palindromic_path(str, a, 0, 0, 3, 3))
+    
